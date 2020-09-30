e5f7a28df47af285248f6ef10010d21608714b45
lnaresh <leelanaresh.b@gmail.com>
2020-09-30 17:01:41 +0530

    2nd revision branch b1

--- string_main.c
+++ string_main.c
@@ -4,13 +4,15 @@ unsigned int mstrlen(char* a)
 {
 char* temp_a=a;
 unsigned int len=0;
-while(temp_a[len])
-	len++;
-return len;
+while(temp_a[len++]);
+
+printf("a=%p\n",a);
+printf("a+len-1=%p,%c\n",a+len-1,*(a+len-2));
+return len-1;
 }
+
 int main()
 {
-
 char a[]="nameofthestring";
 unsigned int len=mstrlen(a);
 printf("%d\n",len);
