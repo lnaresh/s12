42bf9f08aa885c3a907e5b4491032f5ff99c5ef0
lnaresh <leelanaresh.b@gmail.com>
2020-09-30 16:34:41 +0530

    rev1

---
+++ string_main.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+unsigned int mstrlen(char* a)
+{
+char* temp_a=a;
+unsigned int len=0;
+while(temp_a[len])
+	len++;
+return len;
+}
+int main()
+{
+
+char a[]="nameofthestring";
+unsigned int len=mstrlen(a);
+printf("%d\n",len);
+
+}
